980e6a5e65c45c119d8a6a31fa19c632e9aedc6a
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-01 14:37:36 +0530

    allow .env.example in git

--- mern-docker/client-boilerplate/src/components/Home/Home.jsx
+++ mern-docker/client-boilerplate/src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to the Home Page Proceed forward.</h1>
       <p>We are providing the best services for our customers. By adding the different products and showcasing them, we aim to deliver an exceptional shopping experience.</p>
     </div>
   );
