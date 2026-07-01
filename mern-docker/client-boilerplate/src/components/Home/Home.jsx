d9d1852c360011bb3c23a8933900eb64e58aa109
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-01 15:42:33 +0530

    Home changes

--- mern-docker/client-boilerplate/src/components/Home/Home.jsx
+++ mern-docker/client-boilerplate/src/components/Home/Home.jsx
@@ -4,6 +4,8 @@ const Home = () => {
     <div>
       <h1>Welcome to the Home Page Proceed forward.</h1>
       <p>We are providing the best services for our customers. By adding the different products and showcasing them, we aim to deliver an exceptional shopping experience.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident rerum ratione blanditiis. Vel cumque corrupti dolorem dignissimos. Voluptatem dolorum accusamus consequuntur, quaerat ipsum vero ad eligendi reprehenderit tempora quia nulla optio similique nihil reiciendis!</p>
+
     </div>
   );
 };
