78a9d660059dcd2d88c0d64992dafe71cb364e74
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-01 15:31:28 +0530

    fix: add config.js and fix BASE_URL imports 1

--- mern-docker/client-boilerplate/src/App.jsx
+++ mern-docker/client-boilerplate/src/App.jsx
@@ -74,7 +74,7 @@ function App() {
           <nav className="navbar">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link to="/home" className="nav-link">
+                <Link to="/" className="nav-link">
                   Home
                 </Link>
               </li>
