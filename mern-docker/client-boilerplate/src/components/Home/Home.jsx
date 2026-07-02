f52634e35ad6d871c29a51eafc46d1e3b374445e
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-02 12:03:18 +0530

    our story

--- mern-docker/client-boilerplate/src/components/Home/Home.jsx
+++ mern-docker/client-boilerplate/src/components/Home/Home.jsx
@@ -6,6 +6,8 @@ const Home = () => {
       <p>We are providing the best services for our customers. By adding the different products and showcasing them, we aim to deliver an exceptional shopping experience.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident rerum ratione blanditiis. Vel cumque corrupti dolorem dignissimos. Voluptatem dolorum accusamus consequuntur, quaerat ipsum vero ad eligendi reprehenderit tempora quia nulla optio similique nihil reiciendis!</p>
       <h2>About Us</h2>
+      <h3>Our Story</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
 
     </div>
   );
